49a7477a9f81caa314172677daf68ef4fdf96a3a
David Soto <davidsoto94@hotmail.com>
2023-11-25 09:53:45 -0500

    First time working REPL!

---
+++ commandExit.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"errors"
+)
+
+func commandExit() error {
+	return errors.New("Try error")
+}
--- main.go
+++ main.go
@@ -1,9 +1,36 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
+func cleanInput(text string) []string {
+	output := strings.ToLower(text)
+	words := strings.Fields(output)
+	return words
+}
+
 func main() {
-	fmt.Println("Hello World")
+	reader := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("pokedexcli>")
+		reader.Scan()
+		text := cleanInput(reader.Text())
+		if len(text) == 0 {
+			continue
+		}
+		command, ok := getCommands()[text[0]]
+		if !ok {
+			fmt.Println(text, ": command not found")
+			continue
+		}
+		err := command.callback()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
 }
